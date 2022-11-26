21d9028524ce1cc191d898543137b8be74f016f6
BernyFranklin <98352952+BernyFranklin@users.noreply.github.com>
2022-11-26 10:48:08 -0800

    Created shell

---
+++ forshayla/src/main/java/com/generalmnt/App.java
@@ -0,0 +1,24 @@
+package com.generalmnt;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    private static void menu() {
+        // Create scanner object
+        Scanner scan = new Scanner(System.in);
+        // Create Vars
+        char sexSelect = ' ';
+        
+
+    }
+}
